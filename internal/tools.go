590ee041892519c8436eeb32038ce8341c135d54
renanbastos93 <renanbastos.tec@gmail.com>
2023-06-28 22:25:15 -0300

    feat: created method to read weaver.toml and get driver/source

--- internal/tools.go
+++ internal/tools.go
@@ -2,7 +2,10 @@ package internal
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
+
+	"github.com/pelletier/go-toml/v2"
 )
 
 func GetProjectName(dir string) (moduleName string) {
@@ -18,4 +21,49 @@ func GetProjectName(dir string) (moduleName string) {
 	}
 
 	return moduleName
+}
+
+func ReadToml(componentName string) (qsConn string) {
+	pwd, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+
+	weaverToml, err := ioutil.ReadFile(pwd + "/weaver.toml")
+	if err != nil {
+		panic(err)
+	}
+
+	var cfgWeaver map[string]interface{}
+	err = toml.Unmarshal(weaverToml, &cfgWeaver)
+	if err != nil {
+		panic(err)
+	}
+
+	projectName := GetProjectName(pwd)
+	componentModPath := projectName + "/internal/" + componentName + "/Component"
+	for k, v := range cfgWeaver {
+		if k != componentModPath {
+			continue
+		}
+		if value, ok := v.(map[string]interface{}); ok {
+			var driver = ""
+			var source = ""
+			for nk, nv := range value {
+				switch nk {
+				case "Driver":
+					driver = nv.(string)
+				case "Source":
+					source = nv.(string)
+				}
+			}
+			qsConn = fmt.Sprintf("%s://%s", driver, source)
+		}
+	}
+
+	if qsConn == "" {
+		panic("not found settings to running migrate verify your weaver.toml file")
+	}
+
+	return qsConn
 }
