1f4c7585eaf757f6b2e77ca84f63362373258ad3
renanbastos93 <renanbastos.tec@gmail.com>
2023-06-30 01:20:32 -0300

    feat: Added new method for version validation. We recommend updating to the latest version.

--- internal/version.go
+++ internal/version.go
@@ -1,3 +1,29 @@
 package internal
 
-const Version = "0.0.3"
+import (
+	"bytes"
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+const Version = "v0.0.3"
+
+func ValidateLatestVersion() {
+	cmd := exec.Command("go", "list", "-m", "github.com/renanbastos93/boneless@latest")
+	var stdOut bytes.Buffer
+	cmd.Stdout = &stdOut
+	if cmd.Run() != nil {
+		return
+	}
+
+	if result := stdOut.String(); result != "" {
+		_, v, _ := strings.Cut(result, " ")
+		v = v[:len(v)-1]
+		if v != Version {
+			msg := "\033[31mNew version available! Check out our release and update the Boneless!\n"
+			msg += "\033[0m\033[1mMore details: \033[0mhttps://github.com/renanbastos93/boneless/releases/tag/%s\n---\n"
+			fmt.Printf(msg, v)
+		}
+	}
+}
