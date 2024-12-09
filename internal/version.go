9cac4d21a4b418a3ec1a1dc337b31dbe905c5907
renanbastos93 <renanbastos.tec@gmail.com>
2024-12-09 14:23:29 -0300

    fix: check version

--- internal/version.go
+++ internal/version.go
@@ -5,12 +5,14 @@ import (
 	"fmt"
 	"os/exec"
 	"strings"
+
+	"golang.org/x/mod/semver"
 )
 
-const Version = "v0.6.0"
+const Version = "v0.7.0"
 
 func ValidateLatestVersion() {
-	cmd := exec.Command("go", "list", "-m", "github.com/renanbastos93/boneless@latest")
+	cmd := exec.Command(goCLI, "list", "-m", "github.com/renanbastos93/boneless@latest")
 	var stdOut bytes.Buffer
 	cmd.Stdout = &stdOut
 	if cmd.Run() != nil {
@@ -20,10 +22,14 @@ func ValidateLatestVersion() {
 	if result := stdOut.String(); result != "" {
 		_, v, _ := strings.Cut(result, " ")
 		v = v[:len(v)-1]
-		if v != Version {
-			msg := "\033[31mNew version available! Check out our release and update the Boneless!\n"
-			msg += "\033[0m\033[1mMore details: \033[0mhttps://github.com/renanbastos93/boneless/releases/tag/%s\n---\n"
-			fmt.Printf(msg, v)
+
+		// Check if the latest version is greater than the current version
+		if semver.Compare(Version, v) > 0 || v == Version {
+			return
 		}
+
+		msg := "\033[31mNew version available! Check out our release and update the Boneless!\n"
+		msg += "\033[0m\033[1mMore details: \033[0mhttps://github.com/renanbastos93/boneless/releases/tag/%s\n---\n"
+		fmt.Printf(msg, v)
 	}
 }
